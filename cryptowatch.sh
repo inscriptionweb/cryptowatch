885eed1170f4b82e6be3da4cc7d522ea6f595eae
Mike Hicklen <michael.s.hicklen@gmail.com>
2017-09-15 10:01:27 -0500

    New revision with more configurables, better math and a faster API endpoint

--- cryptowatch.sh
+++ cryptowatch.sh
@@ -1,39 +1,59 @@
 ### ethwatch.sh
-### v0.2
-### changelog: decided to use github
+### 20170915
+### v0.3
 
 ### Configurables
 
-# Set the API polling interval - WARNING: Do 
+# Set the API polling interval in seconds - be nice or the API might block you
 __API_INTERVAL=30
-# Set the dollar amount threshold above which we will start beeping
-WATCHPRICE=349
 
+# Set the dollar amount threshold above which we will start beeping
+__WATCH_PRICE=350
+
+# How much you've put into ETH (not required)
+__INVESTED=260.28
+
+# Set the API scraping URL
+__API_URL="https://api.cryptowat.ch/markets/gdax/ethusd/price"
+
+### Start script
+
+echo "API scrape URL: ${__API_URL}"
+
+### Loop through price
 
 while :
 do
     # Scrape the API for the price in USD of 1 ETH
-    ETHAPIPRICE=$(curl -sqk 'https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD' | jq -M '.' | awk '/USD/ {print $2}')
+    ETHAPIPRICE=$(curl -sq ${__API_URL} | awk -F: '/price/ {print $2}' | tr -d '\ ')
     # Ensure 2 decimal places for printing the real price since bash doesn't handle float well
     ETHPRICE=$(echo "scale=2;${ETHAPIPRICE} / 1" | bc)
     # And grab the dollar amount for conditional comparison
 
     ETHDOLLAR=$(echo ${ETHPRICE} | cut -d\. -f1)
 
-    if [[ "${ETHDOLLAR}" -gt ${WATCHPRICE} ]]
-    then
-        beep; beep; beep; beep; beep; beep; beep; beep; beep; beep; beep; beep; beep; beep; beep; beep; beep; beep;
-        echo "ETH is currently \$${ETHPRICE} at $(date) - SELL!"
-    elif [[ "${ETHDOLLAR}" -lt 100 ]]
+    if [[ "${ETHDOLLAR}" -gt ${__WATCH_PRICE} ]]
+
     then
         beep; beep; beep
-        echo "ETH JUST DROPPED HARD - Current price is \$${ETHPRICE} - might want to buy a lot!"
-    elif [[ "${ETHDOLLAR}" -lt 10 ]]
+        echo "$(date): ETH price has exceeded the watch price - current price is \$${ETHPRICE}"
+
+    elif [[ "${ETHDOLLAR}" -lt $(printf %.0f $(echo "scale=0;${ETHAPIPRICE} * 0.50" | bc)) && "${ETHDOLLAR}" -gt $(printf %.0f $(echo "scale=0;${ETHAPIPRICE} * 0.05" | bc)) ]]
+
     then
-        beep; beep; beep; beep; beep; beep;
-        echo "ETH HAS CRASHED - BUY BUY BUY - Current price is \$${ETHPRICE}"
+        beep; beep; beep
+        echo "$(date): ETH price has dropped significantly - current price is \$${ETHPRICE}"
+
+    elif [[ "${ETHDOLLAR}" -lt $(printf %.0f $(echo "scale=0;${ETHAPIPRICE} * 0.75" | bc)) && "${ETHDOLLAR}" -gt $(printf %.0f $(echo "scale=0;${ETHAPIPRICE} * 0.50" | bc)) ]]
+
+    then
+        beep; beep; beep
+        echo "$(date): ETH price is \$${ETHPRICE} - crashed?"
+
     else
-        echo "ETH is \$${ETHPRICE} at $(date). Alarm set to ring after ETH is over \$${WATCHPRICE}."
+        echo "$(date): ETH \$${ETHPRICE} - Alarm when ETH > \$${__WATCH_PRICE}; in for \$${__INVESTED}"
     fi
-    sleep 30
+
+    sleep ${__API_INTERVAL}
+
 done
